b6aff92039715e18006f66d03ee3cee5a69bd090
Martin Atzinov <mapanogg@gmail.com>
2014-04-15 12:47:53 +0300

    Improvements to nested selectors

--- querySelector/document.querySelector.js
+++ querySelector/document.querySelector.js
@@ -10,19 +10,24 @@
         getTag : function(tag, parent) { return parent ? parent.getElementsByTagName(tag) : document.getElementsByTagName(tag) || false },
         find: function(selector, parent) {
             var element, target;
-            if( selector.indexOf(' ') > -1 ) {
-                target = selector.split(' ');
-                target = target[target.length - 1];
-                selector = selector.substr(0, selector.lastIndexOf(' '));
+            if( selector.indexOf ) {
+                if( selector.indexOf(' ') > -1 ) {
+                    target = selector.split(' ');
+                    target = target[target.length - 1];
+                    selector = selector.substr(0, selector.lastIndexOf(' '));
 
-                element = this.drill(selector, target);
+                    element = this.drill(selector, target);
+                }
+                else {
+                    switch( selector.charAt(0) ) {
+                        case '.': element = _helper.getClass( selector.substr(1, selector.length), parent ); break;
+                        case '#': element = _helper.getID( selector.substr(1, selector.length), parent ); break;
+                        default: element = _helper.getTag(selector, parent);
+                    }
+                }
             }
             else {
-                switch( selector.charAt(0) ) {
-                    case '.': element = _helper.getClass( selector.substr(1, selector.length), parent ); break;
-                    case '#': element = _helper.getID( selector.substr(1, selector.length), parent ); break;
-                    default: element = _helper.getTag(selector, parent);
-                }
+                element = this.find(parent, selector);
             }
 
             return element;
